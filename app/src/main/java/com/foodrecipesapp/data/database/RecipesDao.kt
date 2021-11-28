aa87782483026bf94bd3dca910cbb008f00e83d2
BlackHandMinsk <3058079@gmail.com>
2021-11-28 11:49:47 +0300

    recipes database

--- app/src/main/java/com/foodrecipesapp/data/database/RecipesDao.kt
+++ app/src/main/java/com/foodrecipesapp/data/database/RecipesDao.kt
@@ -1,4 +1,4 @@
-package com.foodrecipesapp
+package com.foodrecipesapp.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
--- app/src/main/java/com/foodrecipesapp/data/database/RecipesEntity.kt
+++ app/src/main/java/com/foodrecipesapp/data/database/RecipesEntity.kt
@@ -1,4 +1,4 @@
-package com.foodrecipesapp
+package com.foodrecipesapp.data.database
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
