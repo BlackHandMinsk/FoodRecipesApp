f2c7f02e6e1e691c7854efd656a087cc4f128a12
BlackHandMinsk <3058079@gmail.com>
2021-11-30 18:50:48 +0300

    modify recipes dao

--- app/src/main/java/com/foodrecipesapp/data/database/RecipesDao.kt
+++ app/src/main/java/com/foodrecipesapp/data/database/RecipesDao.kt
@@ -1,11 +1,10 @@
 package com.foodrecipesapp.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.foodrecipesapp.data.database.entities.FavoritesEntity
 import com.foodrecipesapp.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
+import retrofit2.http.DELETE
 
 @Dao
 interface RecipesDao {
@@ -13,6 +12,20 @@ interface RecipesDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend  fun insertRecipes(recipesEntity: RecipesEntity)
 
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
   @Query("SELECT*FROM recipes_table ORDER BY id ASC")
   fun readRecipes(): Flow<List<RecipesEntity>>
+
+
+  @Query("SELECT*FROM favorite_recipes_table ORDER BY id ASC")
+  fun readFavoriteRecipes():Flow<List<FavoritesEntity>>
+
+  @Delete
+  suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+  @Query("DELETE FROM favorite_recipes_table")
+  suspend fun deleteAllFavoriteRecipes()
 }
