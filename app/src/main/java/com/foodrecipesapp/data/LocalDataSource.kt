bec307f655fe6b9cba005d6fc42af42cce7710b8
BlackHandMinsk <3058079@gmail.com>
2021-11-30 18:55:31 +0300

    modify localdatasource

--- app/src/main/java/com/foodrecipesapp/data/LocalDataSource.kt
+++ app/src/main/java/com/foodrecipesapp/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.foodrecipesapp.data
 
 import com.foodrecipesapp.data.database.RecipesDao
+import com.foodrecipesapp.data.database.entities.FavoritesEntity
 import com.foodrecipesapp.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,12 +10,32 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao) {
 
 
-     fun readDatabase(): Flow<List<RecipesEntity>>{
+     fun readRecipes(): Flow<List<RecipesEntity>>{
         return recipesDao.readRecipes()
+    }
+
+
+    fun readFavoriteRecipes():Flow<List<FavoritesEntity>>{
+        return recipesDao.readFavoriteRecipes()
     }
 
 
   suspend fun insertRecipes(recipesEntity: RecipesEntity){
         recipesDao.insertRecipes(recipesEntity)
+    }
+
+
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity){
+        recipesDao.insertFavoriteRecipe(favoritesEntity)
+    }
+
+
+    suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity){
+        recipesDao.deleteFavoriteRecipe(favoritesEntity)
+    }
+
+
+    suspend fun deleteAllFavoriteRecipes(){
+        recipesDao.deleteAllFavoriteRecipes()
     }
 }
