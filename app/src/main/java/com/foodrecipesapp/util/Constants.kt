a51d44dd14ba50fff4b22b9b8a4fe4f19c9d4456
BlackHandMinsk <3058079@gmail.com>
2021-11-30 17:41:16 +0300

    ingredients adapter

--- app/src/main/java/com/foodrecipesapp/util/Constants.kt
+++ app/src/main/java/com/foodrecipesapp/util/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
     companion object{
     const val API_KEY = "db4736cb17dd4c8a986e3a715cf3d781"
     const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
 
         // API Query Keys
         const val QUERY_SEARCH = "query"
