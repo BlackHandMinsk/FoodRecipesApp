793688166168fbc9d22ce3a42110ac7981cbf9cd
BlackHandMinsk <3058079@gmail.com>
2021-11-30 17:57:53 +0300

    ingredients fragment

--- app/src/main/java/com/foodrecipesapp/util/Constants.kt
+++ app/src/main/java/com/foodrecipesapp/util/Constants.kt
@@ -3,9 +3,13 @@ package com.foodrecipesapp.util
 class Constants {
 
     companion object{
+
     const val API_KEY = "db4736cb17dd4c8a986e3a715cf3d781"
     const val BASE_URL = "https://api.spoonacular.com"
         const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
+
+
+        const val RECIPE_RESULT_KEY = "recipeBundle"
 
         // API Query Keys
         const val QUERY_SEARCH = "query"
