801fc728b639645ca1e1d00b23912189a8b96224
BlackHandMinsk <3058079@gmail.com>
2021-12-01 12:52:02 +0300

    action mode title

--- app/src/main/java/com/foodrecipesapp/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/foodrecipesapp/adapters/FavoriteRecipesAdapter.kt
@@ -17,6 +17,8 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity):Recy
 
     private var multiSelection = false
 
+    private lateinit var mActionMode:ActionMode
+
     private var selectedRecipes = arrayListOf<FavoritesEntity>()
     private var myViewHolders = arrayListOf<MyViewHolder>()
     private var favoriteRecipes = emptyList<FavoritesEntity>()
@@ -80,9 +82,11 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity):Recy
         if (selectedRecipes.contains(currentRecipe)){
             selectedRecipes.remove(currentRecipe)
             changeRecipeStyle(holder,R.color.cardBackgroundColor,R.color.strokeColor)
+            applyActionModeTitle()
         }else{
             selectedRecipes.add(currentRecipe)
             changeRecipeStyle(holder,R.color.cardBackgroundLightColor,R.color.colorPrimary)
+            applyActionModeTitle()
         }
     }
 
@@ -92,6 +96,15 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity):Recy
         holder.itemView.favorite_row_cardView.strokeColor = ContextCompat.getColor(requireActivity,strokeColor)
     }
 
+    private fun applyActionModeTitle(){
+        when(selectedRecipes.size){
+            0->{mActionMode.finish()}
+            1->{mActionMode.title = "${selectedRecipes.size} item selected"}
+            else->{mActionMode.title = "${selectedRecipes.size} items selected"}
+
+        }
+    }
+
     override fun getItemCount(): Int {
      return favoriteRecipes.size
     }
@@ -99,6 +112,7 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity):Recy
 
     override fun onCreateActionMode(actionMode: ActionMode?, menu: Menu?): Boolean {
        actionMode?.menuInflater?.inflate(R.menu.favorites_contextual_menu,menu)
+        mActionMode = actionMode!!
         applyStatusBarColor(R.color.contextualStatusBarColor)
         return true
     }
